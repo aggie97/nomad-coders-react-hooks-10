1ea94264fd19de38b32bbe009c5793f1f121693e
aggie97 <4ggie1997@gmail.com>
2022-12-01 20:05:39 +0900

    useInput custom hook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
+import { useInput } from "./customHooks/useInput";
 
 const App = () => {
-  const [item, setItem] = useState(0);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr. ");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="name" {...name} />
     </div>
   );
 };
-
-class AppUpgly extends React.Component {
-  state = {
-    item: 1,
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
-  }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1,
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1,
-      };
-    });
-  };
-}
 
 export default App;
