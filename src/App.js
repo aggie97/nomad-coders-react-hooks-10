b943e1e5f83e5bf1ad92457a5b65d2c49ff0a29f
aggie97 <4ggie1997@gmail.com>
2022-12-01 19:04:07 +0900

    함수형 컴포넌트와 클래스형 컴포넌트의 차이 복습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [item, setItem] = useState(0);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello 1</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 };
+
+class AppUpgly extends React.Component {
+  state = {
+    item: 1,
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>Decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
+}
 
 export default App;
